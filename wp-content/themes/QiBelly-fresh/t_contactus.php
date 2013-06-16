6450c37034ba27d280bb488daf63cc5750491797
Mary Elizabeth Sullivan <beth.mejsullivan@gmail.com>
2013-06-16 10:53:07 -0400

    add contact form

--- wp-content/themes/QiBelly-fresh/t_contactus.php
+++ wp-content/themes/QiBelly-fresh/t_contactus.php
@@ -64,6 +64,56 @@ echo $post->post_content;
 //echo '</pre>';
 //ECHO "HRRO";
 ?>
+	<?php 
+if (isset($_POST["Form"]))
+{
+	import_request_variables("p", "z");
+	$missingfields = array();
+        $required = array("Name"=>"Name", "Email"=>"Email", "Message"=>"Message");	
+	
+        while (list($var, $val) = each($required)) {
+            if (isset($zForm[$var]) && $zForm[$var] != '') {
+                // check this value further here
+            } else {
+                $missingfields[$var] = $val;
+            }
+        }
+	
+	
+        if (count($missingfields)) {
+            print "You missed out one or more fields:<br />";
+
+            while(list($var, $val) = each($missingfields)) {
+                print $val . "<br />";
+            }
+        } else {
+            print "Form passed!<br />";
+            var_dump($zForm);
+		    //$attachments = array(WP_CONTENT_DIR . '/uploads/file_to_attach.zip');
+	   $headers = 'From: My Name <myname@mydomain.com>' . "\r\n";
+	   wp_mail('bethes135@gmail.com', 'subject', 'message', $headers);
+	    
+            exit;
+        }
+	
+	
+}
+?>	    
+<form id="contact-us" method="POST" >
+	
+	<p><label for="txt_name">Name</label><br/>
+	<input type="text" id="txt_name" name="Form[Name]" /></p>
+	
+	<p>
+	<label for="txt_email">Email</label><br/>
+	<input type="text" id="txt_email" name="Form[Email]" /></p>
+	
+	<p>
+	<label for="txt_name">Message</label><br/>
+	<textarea id="txt_message" rows="4" cols="50" name="Form[Message]"></textarea></p>
+	
+	<input type="submit" id="btn_submit" text="Click" />
+</form>
 
             </section>
 <?php get_sidebar('right'); ?>
